a522ccbee282092913e04e6d6d09d349e9e87fba
noahitall <noahitall@gmail.com>
2025-03-13 16:55:13 -0400

    Initial Commit

---
+++ PageMon/PageMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageMonApp.swift
+//  PageMon
+//
+//  Created by Noah Zitsman on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct PageMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
